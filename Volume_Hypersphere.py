d37c77b0e796a06442e3d85df0bcee11f58fd0fc
jspc13 <99113060+jspc13@users.noreply.github.com>
2022-03-31 00:46:34 -0400

    updated
    updated

--- Volume_Hypersphere.py
+++ Volume_Hypersphere.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-def r(x,y):
-    return x**2 + y**2
 rng = np.random.default_rng(12345)
-N = 100
+N = 10
 x = rng.random(N) * 2 - 1
 y = rng.random(N) * 2 - 1
 for i in range(N):
-    r = r(x[i],y[i])
-Integral_answer = 4 / N * r
-print(Integral_answer)
+    function_x_y = 0
+    if x[i]**2 + y[i]**2 <= 1:
+        function_x_y = function_x_y + 1
+    print(function_x_y)
+
+I = (4 / N) * function_x_y
+
+print(I)
--- exercise8_1_Brownian.py
+++ exercise8_1_Brownian.py
@@ -1,27 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 rng = np.random.default_rng(12345)
-L = 101
+L = 101 #lattice
+N = 1000 #steps
 x = np.arange(0,L,1)
 y = np.arange(0,L,1)
-particle_x_value = 50
-particle_y_value = 50
-#move_up = particle_y_value + 1
-#move_down = particle_y_value - 1
-#move_right = particle_x_value + 1
-#move_left = particle_x_value - 1
-#random_direction = rng.choice(4) 
-position = np.arange(0,L,1)
-position_x = position[50,50]
-position_y = position[50,50]
-for i in range(100):
+position_x = 50
+position_y = 50
+new_position_x = np.zeros(N)
+new_position_y = np.zeros(N)
+for i in range(N):
     random_direction = rng.choice(4)
-    if random_direction == 0:
+    if random_direction == 0 and position_y < 100:
         position_y = position_y + 1
-    if random_direction == 1:
+    if random_direction == 1 and position_y < 100:
         position_y = position_y - 1
-    if random_direction == 2:
+    if random_direction == 2 and position_x < 100:
         position_x = position_x + 1
-    if random_direction == 3:
+    if random_direction == 3 and position_x < 100:
         position_x = position_x - 1
-
+    new_position_x[i] = position_x
+    new_position_y[i] = position_y
+plt.scatter(new_position_x, new_position_y)
+plt.show()
