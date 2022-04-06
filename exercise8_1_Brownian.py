8a284a644bd888f253a00e338b153f21337e6b6d
jspc13 <99113060+jspc13@users.noreply.github.com>
2022-04-05 21:14:05 -0400

    UPDATE

--- exercise8_1_Brownian.py
+++ exercise8_1_Brownian.py
@@ -24,4 +24,17 @@ for i in range(100):
         position_x = position_x + 1
     if random_direction == 3:
         position_x = position_x - 1
+<<<<<<< Updated upstream
+=======
+    new_position_x[i] = position_x
+    new_position_y[i] = position_y
+
+print(new_position_x)
+#plt.scatter(new_position_x, new_position_y)
+#plt.show()
+#fig = plt.figure(figsize=(5,5))
+#ax = plt.axes(xlim=(0,100), ylim=(0,100))
+#particle = plt.circle((50,50), radius=0.01)
+#ax.add_patch(particle)
+>>>>>>> Stashed changes
 
