732202bad3b0dfa48b0ee7d1387659805591bb48
jspc13 <99113060+jspc13@users.noreply.github.com>
2022-04-11 14:03:35 -0400

    update

---
+++ exercise8_7_ODE.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def acceleration_along_x(r, rho, C, m, Vx, Vy):
+    return ((-1 * np.pi * r**2 * rho * C * Vx) / (2 * m)) * np.sqrt(Vx**2 + Vy**2)
+
+def acceleration_along_y(g, r, rho, C, m, Vx, Vy):
+    return - g - (((-1 * np.pi * r**2 * rho * C * Vy) / (2 * m)) * np.sqrt(Vx**2 + Vy**2))
+
+def acceleration():
+    m = 1
+    r = 0.08
+    thetta = 30
+    V = 100
+    rho = 1.22
+    C = 0.47
+    Vx = V * np.cos(thetta)
+    Vy = V * np.sin(thetta)
+
+    
