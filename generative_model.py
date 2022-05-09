2609d85134f0718c3e1ac512ce093d4e080729ea
jspc13 <99113060+jspc13@users.noreply.github.com>
2022-05-09 17:22:20 -0400

    class exercise

--- generative_model.py
+++ generative_model.py
@@ -7,10 +7,20 @@ def f(x):
     b = 0
     return m*x + b
 
-rng = np.random.default_rng(12345)
-x = rng.uniform(0,1,50)
-y = f(x) + 0.2*rng.normal(0,1,50)
+N = 100
+i = 0
+new_params_zero = np.zeros(N)
+new_params_one = np.zeros(N)
+for i in range(N):
+    rng = np.random.default_rng()
+    x = rng.uniform(0,1,50)
+    y = f(x) + 0.2 * rng.normal(0,1,50)
+    sigma = np.ones(50) * 0.2
+    params, error_matrix = np.polyfit(x, y, 1, w = 1/sigma, cov=True)
+    new_x = np.linspace(0,1)
+    new_params_zero[i] = params[0]
+    new_params_one[i] = params[1]
 
-plt.scatter(x,y)
+plt.hist(new_params_zero)
+plt.hist(new_params_one)
 plt.show()
-
