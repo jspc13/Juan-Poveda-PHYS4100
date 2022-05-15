89c8700d31b78391e6c526cf98ecb6374a36b7e7
jspc13 <99113060+jspc13@users.noreply.github.com>
2022-05-15 15:12:50 -0400

    updated

---
+++ exercise8_4_ODE_pendulum.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+#define initial conditions
+thetta = 
--- exercise8_7_ODE.py
+++ exercise8_7_ODE.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 h = 0.01 # in seconds
 t_array = np.arange(0,100,h)
 #define initial conditons
-mass = 1
+#mass = 1
 radius = 0.08 
 angle_thetta = np.radians(30) # angle wrt to the horizontal
 Velocity_ball = 100
@@ -16,7 +16,7 @@ y_initial = 0
 Vx_initial = Velocity_ball * np.cos(angle_thetta)
 Vy_initial = Velocity_ball * np.sin(angle_thetta)
 
-def f(r):
+def f(r, mass):
     dx_dt = r[2]
     dy_dt = r[3]
     dVx_dt = ((-1 * np.pi * radius**2 * density_air * coefficient_of_drag * r[2]) / (2 * mass)) * np.sqrt(r[2]**2 + r[3]**2)
@@ -25,14 +25,15 @@ def f(r):
 
 r = [x_initial,y_initial,Vx_initial,Vy_initial]
 r_points = np.zeros((len(t_array),4))
+mass_initial = 1
 
 for i in range(len(t_array)):
+    mass = (mass_initial + i) * 10
     r_points[i] = r
-    k1 = h*f(r)
-    k2 = h*f(r+0.5*k1)
+    k1 = h*f(r, mass)
+    k2 = h*f(r+0.5*k1, mass)
     r = r + k2
 
-print(r_points.shape)
 plt.plot(r_points[:,0],r_points[:,1])
 plt.ylim(bottom=0)
 plt.show()
