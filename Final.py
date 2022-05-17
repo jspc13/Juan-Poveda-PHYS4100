6230ee68820a050ea43340f7c2f155e60b4a87fd
jspc13 <99113060+jspc13@users.noreply.github.com>
2022-05-17 00:29:19 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
 
 #Model Rocket
 #Define constants
@@ -9,10 +8,14 @@ dmass_model_rocketdt = -0.002 #mass burn rate in Kg/s
 thrust_model_rocket = 15 # in N
 drag_coefficient = 0.02
 gravity = 9.8
+gravitational_constant = 6.67e-11 # Newton's Gravitational Constant
+mass_earth = 5.972e24
+radius_earth = 6.37e6
+distance_from_radius = 500 # distance covered by the rocket outside of the Earth, in meters
 v_0 = 0 
 h = 0.01
 vx_initial = 0
-v_initial = 0
+vy_initial = 0
 x_initial = 0
 y_initial = 0
 
@@ -22,20 +25,27 @@ t = np.arange(0,10,h)
 
 def f(r):
     mass_model_rocket = r[0]
-    dydt = r[2]
-    dvdt = 1 / mass_model_rocket * (thrust_model_rocket - mass_model_rocket*gravity - drag_coefficient*r[2]**2 - r[2]*dmass_model_rocketdt)
-    return np.array([dmass_model_rocketdt, dydt, dvdt])
+    dxdt = r[3]
+    dydt = r[4]
+    dvxdt = 1 / mass_model_rocket * ((r[3]/np.sqrt(r[3]**2 + r[4]**2) * thrust_model_rocket) - (drag_coefficient*r[3]*np.sqrt(r[3]**2 + r[4]**2)) - (r[3]*dmass_model_rocketdt))
+    dvydt = 1 / mass_model_rocket * ((r[4]/np.sqrt(r[3]**2 + r[4]**2) * thrust_model_rocket) - (gravitational_constant*((mass_model_rocket*mass_earth)/(radius_earth+distance_from_radius)**2)) - (drag_coefficient*r[4]*np.sqrt(r[3]**2 + r[4]**2)) - (r[4]*dmass_model_rocketdt))
+    return np.array([dmass_model_rocketdt, dxdt, dydt, dvxdt, dvydt])
 
-r = [initial_mass_model_rocket, y_initial, v_initial]
-r_points = np.zeros((len(t),3))
 
-for i in range(len(t)):
-    r_points[i] = r
-    k1 = h*f(r)
-    k2 = h*f(r+0.5*k1)
-    r = r + k2
+def runge_kutta():
+    r = [initial_mass_model_rocket, x_initial, y_initial, vx_initial, vy_initial]
+    r_points = np.zeros((len(t),5))
+
+    for i in range(len(t)):
+        r_points[i] = r
+        k1 = h*f(r)
+        k2 = h*f(r+0.5*k1)
+        r = r + k2
+    return r_points
 
 plt.plot(t, r_points[:,2])
 plt.ylim(bottom=0)
 #plt.plot(t,v)
-plt.show()
+#plt.show()
+
+#need to make runga a function, add the mass argument just like for the lab exercise. Maybe also would be good to add thrust, gravity and drag as arguments of the f(x) function
